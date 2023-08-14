0e5cd8afa5a373aa681269ac53b82ec466f48421
ActionR-13148 <142018012+ActionR-13148@users.noreply.github.com>
2023-08-14 14:17:02 +0800

    Add files via upload

---
+++ loss.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+class LabelSmoothingCrossEntropy(nn.Module):
+    def __init__(self, smoothing=0.01):
+        super(LabelSmoothingCrossEntropy, self).__init__()
+        self.smoothing = smoothing
+
+    def forward(self, x, target):
+        confidence = 1. - self.smoothing
+        logprobs = F.log_softmax(x, dim=-1)
+        nll_loss = -logprobs.gather(dim=-1, index=target.unsqueeze(1))
+        nll_loss = nll_loss.squeeze(1)
+        smooth_loss = -logprobs.mean(dim=-1)
+        loss = confidence * nll_loss + self.smoothing * smooth_loss
+        return loss.mean()
+
+def pairwise_distances(x):
+    # x should be two dimensional
+    instances_norm = torch.sum(x ** 2, -1).reshape((-1, 1))
+    return -2 * torch.mm(x, x.t()) + instances_norm + instances_norm.t()
+
+
+def GaussianKernelMatrix(x, sigma=1):
+    pairwise_distances_ = pairwise_distances(x)
+    return torch.exp(-pairwise_distances_ / sigma)
+
+
+def HSIC(x, y, s_x=1, s_y=1):
+    m, _ = x.shape  # batch size
+    K = GaussianKernelMatrix(x, s_x)
+    L = GaussianKernelMatrix(y, s_y)
+    H = torch.eye(m) - 1.0 / m * torch.ones((m, m))
+    H = H.float().cuda()
+    HSIC = torch.trace(torch.mm(L, torch.mm(H, torch.mm(K, H)))) / ((m - 1) ** 2)
+    return HSIC
+
+
+def loss_func(z, z_prior, y, num_cls):
+
+    y_valid = [i_cls in y for i_cls in range(num_cls)]
+    z_mean = torch.stack([z[y == i_cls].mean(dim=0) for i_cls in range(num_cls)], dim=0)  # 60 256
+    hsic = HSIC(z_mean[y_valid], z_prior[y_valid].to(z.device))
+    return hsic
+
+
